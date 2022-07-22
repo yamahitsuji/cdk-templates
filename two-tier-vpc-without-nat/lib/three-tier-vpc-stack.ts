a0c57fe0cb2052f6ce47f234fca8109bedc1af22
Yamahitsuji <goat06131004@gmail.com>
2022-07-22 12:03:39 +0900

    adding two tier vpc without nat

---
+++ two-tier-vpc-without-nat/lib/three-tier-vpc-stack.ts
@@ -0,0 +1,28 @@
+import { Stack, StackProps } from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
+
+export class TwoTierVpcWithoutNatStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props)
+
+    const vpcCidr = '10.0.0.0/16'
+
+    const vpc = new ec2.Vpc(this, 'VPC', {
+      cidr: vpcCidr,
+      maxAzs: 3,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'public',
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
+          name: 'private',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+        },
+      ],
+    })
+  }
+}
